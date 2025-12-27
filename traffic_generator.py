18444198d9ae2165236b5d4dc68a2838dad79f22
RaRa <fkrrvwfrgt@privaterelay.appleid.com>
2025-12-27 11:22:15 +0545

    Added traffic generator script

--- traffic_generator.py
+++ traffic_generator.py
@@ -7,13 +7,11 @@ roads = ['A', 'B', 'C', 'D']
 while True:
     road = random.choice(roads)
     lane = random.randint(1, 3)
-
     filename = f"lane{road}.txt"
 
     with open(filename, "a") as f:
-        f.write(f"{vehicle_id},{lane}\n")
+    f.write(f"{vehicle_id},{lane}\n")
 
     print(f"Generated Vehicle {vehicle_id} on Road {road} Lane {lane}")
     vehicle_id += 1
-
     time.sleep(1)
