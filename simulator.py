752c1e765c4798b6083c830953a51eda0bcf6f8d
RaRa <fkrrvwfrgt@privaterelay.appleid.com>
2025-12-27 11:25:17 +0545

    Implemented traffic simulation with priority lane logic
    Updated traffic simulator to utilize a dictionary for lane management, improving scalability and readability. The code now dynamically handles multiple lanes instead of a single queue.

--- simulator.py
+++ simulator.py
@@ -1,27 +1,36 @@
 import time
 from queue_ds import Queue
 
-AL2 = Queue()
+lanes = {
+    "AL2": Queue(), 
+    "BL2": Queue(),
+    "CL2": Queue(),
+    "DL2": Queue()
+}
 
 vehicle_counter = 0
-
-print("Traffic Simulator Started...\n")
+print("Traffic Simulator Started\n")
 
 while True:
-  
-    vehicle_counter += 1
-    AL2.enqueue(vehicle_counter)
+   vehicle_counter += 1
+    lanes["AL2"].enqueue(vehicle_counter)  
     print(f"Vehicle {vehicle_counter} arrived in AL2")
 
- 
-    if AL2.size() > 10:
-        print("\n PRIORITY MODE ACTIVATED")
 
-        while AL2.size() >= 5:
-            served_vehicle = AL2.dequeue()
-            print(f"Serving vehicle {served_vehicle} from AL2")
+    if lanes["AL2"].size() > 10:
+        print("\n PRIORITY MODE: AL2")
+        while lanes["AL2"].size() >= 5:
+            served = lanes["AL2"].dequeue()
+            print(f"Serving Vehicle {served} from AL2")
             time.sleep(1)
+        print("Priority cleared. Normal traffic resumes.\n")
 
-        print("Priority cleared, returning to normal mode\n")
+   
+    else:
+        for lane_name in lanes:
+            if not lanes[lane_name].is_empty():
+                served = lanes[lane_name].dequeue()
+                print(f"Serving Vehicle {served} from {lane_name}")
+                break
 
     time.sleep(1)
